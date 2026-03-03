9177f8255b076aca10c989d3b1a12b0e81ef205c
basiia <vara_spirudonova@gmail.com>
2026-03-03 12:37:56 +0300

    Dodano Tickets

--- obj/Debug/net8.0/UniDesc.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/UniDesc.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniDesc.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb3d1df879b7f81ac1bac23e52953ee615efa57")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniDesc.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniDesc.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
